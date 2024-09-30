d1e10f76e045165c3e2544a42ffae2f6d4f1c47d
Rubert Gonzalez <rubertgonzalez5@gmail.com>
2024-09-29 22:28:11 -0500

    🆕 Card contenedor

--- src/app/dashboard/card/page.tsx
+++ src/app/dashboard/card/page.tsx
@@ -12,7 +12,7 @@ import { Label } from "@/components/ui/label";
 
 export default function Page() {
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-4">
       {
         Array.from({ length: 10 }).map((_, index) => (
           <Card key={index}>
