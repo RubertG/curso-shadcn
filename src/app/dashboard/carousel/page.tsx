81783a826e8008fa7a0780aa4134b52db5984927
Rubert Gonzalez <rubertgonzalez5@gmail.com>
2024-09-30 14:03:25 -0500

    🆕 Carrusel y autoplay

---
+++ src/app/dashboard/carousel/page.tsx
@@ -0,0 +1,69 @@
+"use client"
+
+import { Card, CardContent } from "@/components/ui/card"
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel"
+
+export default function Page() {
+  return (
+    <div className="grid gap-4">
+      <div className="w-full flex items-center justify-center px-10">
+        <Carousel
+          className="w-full"
+          opts={{
+            align: "start",
+            loop: true
+          }}
+        >
+          <CarouselContent>
+            {Array.from({ length: 5 }).map((_, index) => (
+              <CarouselItem key={index} className="md:basis-1/2 lg:basis-1/3">
+                <div className="p-1">
+                  <Card>
+                    <CardContent className="flex aspect-square items-center justify-center p-6">
+                      <span className="text-4xl font-semibold">{index + 1}</span>
+                    </CardContent>
+                  </Card>
+                </div>
+              </CarouselItem>
+            ))}
+          </CarouselContent>
+          <CarouselPrevious className="hidden sm:flex" />
+          <CarouselNext className="hidden sm:flex" />
+        </Carousel>
+      </div>
+
+      <div className="w-full flex items-center justify-center px-10">
+        <Carousel
+          className="w-full"
+          autoplay={3000}
+          opts={{
+            align: "start",
+            loop: true
+          }}
+        >
+          <CarouselContent>
+            {Array.from({ length: 5 }).map((_, index) => (
+              <CarouselItem key={index} className="md:basis-1/2 lg:basis-1/3">
+                <div className="p-1">
+                  <Card>
+                    <CardContent className="flex aspect-square items-center justify-center p-6">
+                      <span className="text-4xl font-semibold">{index + 1}</span>
+                    </CardContent>
+                  </Card>
+                </div>
+              </CarouselItem>
+            ))}
+          </CarouselContent>
+          <CarouselPrevious className="hidden sm:flex" />
+          <CarouselNext className="hidden sm:flex" />
+        </Carousel>
+      </div>
+    </div>
+  );
+}
