ffbd8e42fe25d2f38de2a0d391990501f128dd52
Rubert Gonzalez <rubertgonzalez5@gmail.com>
2024-10-01 11:32:36 -0500

    🆕 Command

---
+++ src/app/dashboard/command/page.tsx
@@ -0,0 +1,80 @@
+"use client"
+
+import { CommandEmpty, CommandGroup, CommandInput, CommandItem, CommandList, CommandSeparator, CommandShortcut, CommandDialog } from "@/components/ui/command";
+import { FaceIcon, PersonIcon, EnvelopeClosedIcon, GearIcon, CalendarIcon, RocketIcon } from "@radix-ui/react-icons";
+import { useEffect, useState } from "react";
+
+export default function Page() {
+  const [open, setOpen] = useState(false)
+
+  useEffect(() => {
+    const onkeyDown = (e: KeyboardEvent) => {
+      if (e.key === "j" && (e.metaKey || e.ctrlKey)) {
+        e.preventDefault()
+        setOpen((open) => !open)
+      }
+    }
+ 
+    document.addEventListener("keydown", onkeyDown)
+    return () => document.removeEventListener("keydown", onkeyDown)
+  }, [])
+
+  return (
+    <div>
+      <CommandDialog
+        open={open}
+        onOpenChange={setOpen}
+      >
+        <CommandInput placeholder="Type a command or search..." />
+        <CommandList>
+          <CommandEmpty>No results found.</CommandEmpty>
+          <CommandGroup heading="Suggestions">
+            <CommandItem>
+              <CalendarIcon className="mr-2 h-4 w-4" />
+              <span>Calendar</span>
+            </CommandItem>
+            <CommandItem>
+              <FaceIcon className="mr-2 h-4 w-4" />
+              <span>Search Emoji</span>
+            </CommandItem>
+            <CommandItem disabled>
+              <RocketIcon className="mr-2 h-4 w-4" />
+              <span>Launch</span>
+            </CommandItem>
+          </CommandGroup>
+          <CommandSeparator />
+          <CommandGroup heading="Settings">
+            <CommandItem>
+              <PersonIcon className="mr-2 h-4 w-4" />
+              <span>Profile</span>
+              <CommandShortcut>⌘P</CommandShortcut>
+            </CommandItem>
+            <CommandItem>
+              <EnvelopeClosedIcon className="mr-2 h-4 w-4" />
+              <span>Mail</span>
+              <CommandShortcut>⌘B</CommandShortcut>
+            </CommandItem>
+            <CommandItem>
+              <GearIcon className="mr-2 h-4 w-4" />
+              <span>Settings</span>
+              <CommandShortcut>⌘S</CommandShortcut>
+            </CommandItem>
+          </CommandGroup>
+        </CommandList>
+      </CommandDialog>
+
+      <div className="flex items-center justify-center">
+        <p className="text-sm text-muted-foreground">
+          Press{" "}
+          <kbd className="pointer-events-none inline-flex h-5 select-none items-center gap-1 rounded border bg-muted px-1.5 font-mono text-[10px] font-medium text-muted-foreground opacity-100">
+            <span className="text-xs">⌘</span>J
+          </kbd>
+          {" "} or {" "}
+          <kbd className="pointer-events-none inline-flex h-5 select-none items-center gap-1 rounded border bg-muted px-1.5 font-mono text-[10px] font-medium text-muted-foreground opacity-100">
+            <span className="text-xs">Ctrl</span> + J
+          </kbd>
+        </p>
+      </div>
+    </div>
+  );
+}
